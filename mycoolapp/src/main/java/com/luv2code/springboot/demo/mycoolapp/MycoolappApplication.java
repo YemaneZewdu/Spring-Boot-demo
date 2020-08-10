1327a4673fe5c08b0d88fdc5282f84e2246045cf
YemaneZewdu <yemane18berhan@gmail.com>
2020-08-09 22:23:41 -0400

    first commit

---
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// This project is from Spring Boot project starter website
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication.class, args);
+	}
+}
